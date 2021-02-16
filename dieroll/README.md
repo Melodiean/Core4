41a9ac2902cc5c6f49eab6c03c4e912ec9fcc1c8
Melodiean <melodynsita@gmail.com>
2021-02-16 16:08:21 +0300

    Roll the dice

---
+++ dieroll/README.md
@@ -0,0 +1,5 @@
+# Getting Started with Create React App
+
+create a react app from scratch
+no dependencies
+use webpack and babel
---
+++ dieroll/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Roll from "./Roll";
+
+function App() {
+  return (
+    <div>
+      <h1>Roll the Dice</h1>
+      <Roll />{" "}
+    </div>
+  );
+}
+
+export default App;
